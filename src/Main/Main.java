07076e5d9dedefccc1fb49d44132636dc872205d
Moritz Kronberger <moritzkronberger@aon.at>
2020-10-08 12:03:19 +0200

    Scanner and Variables created

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,11 +6,20 @@
 
 package Main;
 
+import java.util.Scanner;
+
 public class Main
 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
     }
 
 }
