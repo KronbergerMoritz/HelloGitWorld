37ed9aa44f6797ba12344fbd116c24113df9e90f
Moritz Kronberger <moritzkronberger@aon.at>
2020-10-08 12:27:36 +0200

    Finish the Project

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,25 +11,33 @@ import java.util.Scanner;
 public class Main
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Geben Sie bitte 2 Werte ein:");
 
-        try {
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
+        boolean cond = false;
 
-            int i = num1 + num2;
-            System.out.println(i);
+
+                try {
+
+                    int num1 = sc.nextInt();
+                    int num2 = sc.nextInt();
+
+                    int i = num1 + num2;
+
+                    cond = true;
+
+
+                    System.out.println("Das Ergebnis Lautet: " + i);
+                } catch (Exception e) {
+                    System.out.println("Nochmal!");
+                }
+            }
+
         }
-        catch(Exception e)
-        {
-            
-        }
 
-    }
 
-}
+
+
